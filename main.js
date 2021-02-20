bd12e315d26cf92b784c2ba9ce55d7298c6f9a76
Mateusz Olejnik <olejuss@gmail.com>
2021-02-20 22:27:05 +0100

    add final changes .js/.html

--- main.js
+++ main.js
@@ -2,18 +2,28 @@ const button = document.querySelector("button");
 const characters = `QWERTYUIOPASDFGHJKLZXCVBNM1234567890`;
 const section = document.createElement("section")
 
+const codeLenght = 10
+const codesNumber = 10
+
+
 const init = () => {
 
     document.body.appendChild(section)
     section.style.width = "100vw"
     section.style.marginTop = "20vh"
-
 }
 
+
 const genCode = () => {
+
     console.log("works");
-    for (i = 0; i < 10; i++) {
-        const code = "kod"
+
+    for (let i = 0; i < codesNumber; i++) {
+        let code = "";
+        for (let i = 0; i < codeLenght; i++) {
+            const index = Math.floor(Math.random() * 36)
+            code += characters[index]
+        }
         const div = document.createElement("div")
         div.textContent = code
         section.appendChild(div)
@@ -21,7 +31,6 @@ const genCode = () => {
         div.style.margin = "0 auto"
         div.style.textAlign = "center"
     }
-
 }
 
 button.addEventListener(`click`, genCode)
